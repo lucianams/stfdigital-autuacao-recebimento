6a5e3c4a4d83f6936d7b98e4f76c975aa9a0c799
viniciusksilva <viniciusksilva@gmail.com>
2016-05-02 16:37:27 -0300

    teste iniciar processo fisico

--- src/ui/test/e2e/peticaofisica.scenario.ts
+++ src/ui/test/e2e/peticaofisica.scenario.ts
@@ -3,10 +3,15 @@ declare var require;
 describe('Autuação de Petições Físicas Originárias', () => {
     
     var loginPage : LoginPage = require('./pages/login.page');
+    var principaPage : PrincipalPage = require('./pages/principal.page');
                 
     it ('Deveria logar na tela', () => {
         loginPage.open();
         loginPage.login('aaa', '123');
+    });
+    
+    it ('Deveria acessar a pagina de peticao física', () => {
+        principalPage.iniciarProcesso();
     });
     
 });

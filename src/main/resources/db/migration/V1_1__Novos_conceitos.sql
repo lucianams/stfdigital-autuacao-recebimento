d34f3cf37189ff27d1780584436ce0d3bd3b568c
Rafael.Alencar <rafaelesdras@gmail.com>
2016-05-11 16:48:33 -0300

    Correção de chaves estrangeiras e tamanho de campo.

--- src/main/resources/db/migration/V1_1__Novos_conceitos.sql
+++ src/main/resources/db/migration/V1_1__Novos_conceitos.sql
@@ -1,4 +1,4 @@
-create table recebimento.classe_peticionavel (sig_classe varchar2(6) not null, nom_classe varchar2(100) not null, constraint pk_classe_peticionavel primary key (sig_classe));
+create table recebimento.classe_peticionavel (sig_classe varchar2(6) not null, nom_classe varchar2(80) not null, constraint pk_classe_peticionavel primary key (sig_classe));
 
 create table recebimento.preferencia (seq_preferencia number not null, nom_preferencia varchar2(100) not null, constraint pk_preferencia primary key (seq_preferencia));
 
@@ -20,7 +20,7 @@ alter table recebimento.motivo_devolucao add constraint uk_mode_dsc_motivo_devol
 create table recebimento.motivo_tipo_documento (seq_motivo_devolucao number not null, seq_tipo_documento number not null, constraint pk_motivo_tipo_documento primary key (seq_motivo_devolucao, seq_tipo_documento));
 alter table recebimento.motivo_tipo_documento add constraint fk_motivo_devolucao_motd foreign key (seq_motivo_devolucao) references recebimento.motivo_devolucao(seq_motivo_devolucao);
 
-create table recebimento.devolucao (seq_protocolo number not null, dsc_motivacao varchar2(1000) not null, seq_motivo_devolucao number, seq_modelo_documento number, seq_texto number, constraint pk_devolucao primary key (seq_protocolo));
+create table recebimento.devolucao (seq_protocolo number not null, dsc_motivacao varchar2(250) not null, seq_motivo_devolucao number, seq_modelo_documento number, seq_texto number, constraint pk_devolucao primary key (seq_protocolo));
 alter table recebimento.devolucao add constraint fk_remessa_devo foreign key (seq_protocolo) references recebimento.remessa(seq_protocolo);
 alter table recebimento.devolucao add constraint fk_motivo_devolucao_devo foreign key (seq_motivo_devolucao) references recebimento.motivo_devolucao(seq_motivo_devolucao);
 alter table recebimento.devolucao add constraint fk_modelo_devolucao_devo foreign key (seq_modelo_documento) references recebimento.modelo_devolucao(seq_modelo_documento);
@@ -28,3 +28,4 @@ alter table recebimento.devolucao add constraint fk_modelo_devolucao_devo foreig
 alter table recebimento.remessa drop column dsc_motivo;
 alter table recebimento.remessa add column sig_recebedor varchar2(30) not null;
 alter table recebimento.remessa add column dat_recebimento date not null;
+alter table recebimento.remessa add constraint fk_classe_peticionavel_reme foreign key (sig_classe) references recebimento.classe_peticionavel(sig_classe);

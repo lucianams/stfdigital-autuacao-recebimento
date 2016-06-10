28962f7ededec38ebcc5ca5fd933de34ab68a2c8
Tomás Godoi <tsmgodoi@gmail.com>
2016-06-10 00:04:58 -0300

    Adicionando arquivo system.conf.js, que havia faltado. Mudando o .gitignore para não ignorar o system.conf.js.

---
+++ src/test/ui/system.conf.js
@@ -0,0 +1,8 @@
+System.config({
+	transpiler: null,
+	map: {
+		'systemjs': './node_modules/systemjs/dist/system.js',
+		'system-polyfills': './node_modules/systemjs/dist/system-polyfills.js'
+	},
+	strictImportSequence: true
+});

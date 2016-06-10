d8a9a4add59fc23cbe38b5ae986635ea5c7464cb
Tomas.Godoi <tomas.godoi@stf.jus.br>
2016-06-10 20:28:56 -0300

    Criando a estrutura básica da funcionalidade de assinatura de ofício de devolução.

---
+++ src/main/ui/app/devolucao-assinatura.ts
@@ -0,0 +1,6 @@
+import devolucaoAssinatura from "./devolucao-assinatura/devolucao-assinatura.module";
+import "./devolucao-assinatura/devolucao-assinatura.controller";
+import "./devolucao-assinatura/devolucao-assinatura.service";
+import "./services";
+
+export default devolucaoAssinatura;
---
+++ src/main/ui/app/devolucao-assinatura/devolucao-assinatura.module.ts
@@ -0,0 +1,33 @@
+import ITranslatePartialLoaderService = angular.translate.ITranslatePartialLoaderService;
+import IStateProvider = angular.ui.IStateProvider;
+import IModule = angular.IModule;
+import {Remessa} from "./../services/model";
+
+/** @ngInject **/
+function config($stateProvider: IStateProvider, properties: any) {
+
+    $stateProvider.state('app.novo-processo.devolucao-assinatura', {
+        url : '/devolucao-assinatura',
+        views : {
+            'content@app.autenticado' : {
+                templateUrl : './devolucao-assinatura.tpl.html',
+                controller : 'app.novo-processo.devolucao-assinatura.DevolucaoAssinaturaController',
+                controllerAs: 'vm'
+            }
+        },
+        resolve : {
+            remessas: () => {
+            	return [new Remessa('a', 1, 2, 'b', 'c'), new Remessa('b', 1, 2, 'b', 'c')];
+            },
+        }
+    });
+}
+
+/** @ngInject **/
+function run($translatePartialLoader: ITranslatePartialLoaderService, properties: any) {
+	$translatePartialLoader.addPart(properties.apiUrl + '/recebimento/devolucao-assinatura');
+}
+
+let devolucaoAssinatura: IModule = angular.module('app.novo-processo.devolucao-assinatura', ['app.novo-processo.preautuacao-services', 'app.novo-processo', 'app.constants', 'app.messaging']);
+devolucaoAssinatura.config(config).run(run);
+export default devolucaoAssinatura;

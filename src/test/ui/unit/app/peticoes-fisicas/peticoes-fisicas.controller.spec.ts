1544d30e22ac481840130694b777afa53c21b2d6
viniciusksilva <viniciusksilva@gmail.com>
2016-06-21 15:37:13 -0300

    Teste unitario do front-end do registro de petição fisica criado

--- src/test/ui/unit/app/peticoes-fisicas/peticoes-fisicas.controller.spec.ts
+++ src/test/ui/unit/app/peticoes-fisicas/peticoes-fisicas.controller.spec.ts
@@ -1,16 +1,53 @@
 import {PeticaoFisicaController} from "recebimento/peticoes-fisicas/peticao-fisica.controller";
-import {PeticaoFisicaService} from "recebimento/peticoes-fisicas/peticao-fisica.service";
+import {PeticaoFisicaService, PeticaoFisicaCommand, FormaRecebimento} from "recebimento/peticoes-fisicas/peticao-fisica.service";
 
-describe('Teste do controlador peticoes-fisicas.controller', () => {
-	let controller : PeticaoFisicaController;
-	var scope;
+
+describe('Teste do controlador peticao-fisica.controller', () => {
 	
-	beforeEach(inject(($rootScope, $httpBackend: angular.IHttpBackendService, $controller: angular.IControllerService, properties : any) => {
-		$httpBackend.expectGET(properties.apiUrl + '/formas-recebimento').respond([{sigla: 'BALCAO', descricao: 'Balcão', exigeNumero : false}, {sigla: 'SEDEX', descricao: 'Sedex', exigeNumero : true}]);
-		let ctrl = new PeticaoFisicaController(null, null, null);
+	let controller : PeticaoFisicaController;
+	let $q : ng.IQService;
+	let $rootScope : ng.IRootScopeService;
+	let mockState;
+	let mockPeticaoFisicaService;
+	
+	beforeEach(inject((_$q_, _$rootScope_) => {
+        $q = _$q_;
+        $rootScope = _$rootScope_;
 	}));
 	
-	it ('Deveria carregar a lista de forma de envio do controlador', () =>{
-		expect(controller.formasRecebimento.length).toEqual(2);
+	beforeEach(() => {
+		mockState = {
+			go : () => {}
+		};
+		mockPeticaoFisicaService = {
+			registrar : () =>{}
+		}
+	    controller = new PeticaoFisicaController(mockState, mockPeticaoFisicaService, [new FormaRecebimento('MALOTE', 'Malote', false)]);
 	});
+	
+	it('Deveria registrar o recebimento da petição física', () => {
+		let formaRecebimento = 'MALOTE';
+		let qtdApensos = 3;
+		let qtdVolumes = 2
+		let numeroSedex : string;
+		let tipoProcesso = 'ORIGINARIO';
+		
+		controller.formaRecebimento = formaRecebimento;
+		controller.qtdApensos = qtdApensos;
+		controller.qtdVolumes = qtdVolumes;
+		controller.tipoProcesso = tipoProcesso;
+		
+		spyOn(mockPeticaoFisicaService, 'registrar').and.callFake(() => $q.when());
+		
+		spyOn(mockState, 'go').and.callThrough();
+		
+		controller.registrarPeticao();
+		
+		$rootScope.$apply();
+		
+		expect(mockPeticaoFisicaService.registrar).toHaveBeenCalledWith(new PeticaoFisicaCommand(formaRecebimento, qtdVolumes, qtdApensos, numeroSedex, tipoProcesso));
+		
+		expect(mockState.go).toHaveBeenCalledWith("app.tarefas.minhas-tarefas", {}, { reload: true	});
+	});
+	
 });

8f64230cd8aa05ce52e80dca0288833e5bae8d5e
viniciusksilva <viniciusksilva@gmail.com>
2016-06-23 23:18:06 -0300

    Mecanismo de commando

--- src/main/ui/app/peticoes-fisicas/peticao-fisica.service.ts
+++ src/main/ui/app/peticoes-fisicas/peticao-fisica.service.ts
@@ -3,6 +3,7 @@ import IPromise = angular.IPromise;
 import IHttpPromiseCallbackArg = angular.IHttpPromiseCallbackArg;
 import recebimento from "./peticao-fisica.module";
 import cmd = app.support.command;
+import Properties = app.support.constants.Properties;
 
 
 export class FormaRecebimento {
@@ -12,12 +13,15 @@ export class FormaRecebimento {
 
 export class PeticaoFisicaCommand implements cmd.Command {
     
-    constructor(public formaRecebimento: string, 
-                public volumes: number,
-                public apensos: number,
-                public numeroSedex: string,
-                public tipoProcesso: string,
-                public sigilo: string = 'PUBLICO') { }
+	public formaRecebimento: string;
+    public volumes: number;
+    public apensos: number;
+    public numeroSedex: string;
+    public tipoProcesso: string;
+    public sigilo: string = 'PUBLICO';
+
+	constructor() {}
+
 }
 
 export class PeticaoFisicaService {
@@ -25,7 +29,9 @@ export class PeticaoFisicaService {
     private static apiRemessa: string = '/recebimento/api/remessas';
 
     /** @ngInject **/
-    constructor(private $http: IHttpService, private properties) { }
+    constructor(private $http: IHttpService, private properties: Properties, commandService: cmd.CommandService) { 
+    	commandService.setValidator('registrar-remessa', new ValidadorRemessa());
+    }
 
     public registrar(peticao: PeticaoFisicaCommand): IPromise<any> {
         return this.$http.post(this.properties.url + ":" + this.properties.port + PeticaoFisicaService.apiRemessa + '/recebimento', peticao);
@@ -39,12 +45,34 @@ export class PeticaoFisicaService {
     }
 }
 
-export class ValidadorRemessa implements cmd.CommandValidator {
+class ValidadorRemessa implements cmd.CommandValidator {
 	
 	constructor() {}
 	
 	public isValid(command: PeticaoFisicaCommand): boolean {
-		return command.numeroSedex != "";
+		if (angular.isNumber(command.apensos) &&
+			angular.isNumber(command.volumes) &&
+			this.validarTipoProcesso(command.tipoProcesso) &&
+			this.validarFormaRecebimento(command.formaRecebimento, command.numeroSedex)) {
+			return true;
+		}
+		return false;
+	}
+	
+	private validarTipoProcesso(tipoProcesso: string): boolean {
+		if (tipoProcesso === "ORIGINARIO" || tipoProcesso === "RECURSAL") {
+			return true;
+		}
+		return false;
+	}
+	
+	private validarFormaRecebimento(siglaFormaRecebimento: string, numeroSedex: string): boolean {
+		if (siglaFormaRecebimento && 
+				((siglaFormaRecebimento === "SEDEX" && numeroSedex) ||
+				 (siglaFormaRecebimento !== "SEDEX" && !numeroSedex))) {
+			return true;
+		}
+		return false;
 	}
 }
 

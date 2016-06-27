6a70158ee194de579df26323a0e60defea60d655
Tomas.Godoi <tomas.godoi@stf.jus.br>
2016-06-27 20:12:48 -0300

    Configurando docker no travis.

--- src/test/java/br/jus/stf/autuacao/recebimento/ConsultasRemessaIntegrationTests.java
+++ src/test/java/br/jus/stf/autuacao/recebimento/ConsultasRemessaIntegrationTests.java
@@ -1,5 +1,12 @@
 package br.jus.stf.autuacao.recebimento;
 
-public class ConsultasRemessaIntegrationTests {
+import org.junit.Ignore;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+
+import br.jus.stf.core.framework.testing.IntegrationTestsSupport;
+
+@Ignore
+@SpringApplicationConfiguration(ApplicationContextInitializer.class)
+public class ConsultasRemessaIntegrationTests extends IntegrationTestsSupport {
 
 }

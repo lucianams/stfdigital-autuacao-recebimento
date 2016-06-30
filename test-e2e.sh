2151f525bbfbdc2e9bf59156d079c453e44f230c
Tomas.Godoi <tomas.godoi@stf.jus.br>
2016-06-30 20:45:27 -0300

    Alterando a permissão.

--- test-e2e.sh
+++ test-e2e.sh
@@ -4,5 +4,5 @@ MAIN_DOCKER_COMPOSE_FILE="-f docker-compose.yml"
 COMPOSE_FILES_PARAMS="$MAIN_DOCKER_COMPOSE_FILE -f docker-compose-e2e.yml"
 
 docker-compose $COMPOSE_FILES_PARAMS up -d
-./wait-up.sh "https://docker:8765/recebimento/info" 600
+./wait-up.sh "https://localhost:8765/recebimento/info" 600
 docker-compose $COMPOSE_FILES_PARAMS down

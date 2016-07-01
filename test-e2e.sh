924937f71cce7bf06bbaba2d41ccd743d5c8f6e6
Tomas.Godoi <tomas.godoi@stf.jus.br>
2016-07-01 00:09:30 -0300

    Tentativa de rodar os testes e2e no travis-ci.

--- test-e2e.sh
+++ test-e2e.sh
@@ -7,6 +7,10 @@ COMPOSE_FILES_PARAMS="$MAIN_DOCKER_COMPOSE_FILE -f docker-compose-e2e.yml"
 docker run -d --name=grid -p 4444:24444 -p 5900:25900 -e VNC_PASSWORD=hola -v /dev/shm:/dev/shm elgalu/selenium:2.53.0t
 docker exec grid wait_all_done 30s
 
-#docker-compose $COMPOSE_FILES_PARAMS up -d
-#./wait-up.sh "https://docker:8765/recebimento/info" 600
-#docker-compose $COMPOSE_FILES_PARAMS down
+docker-compose $COMPOSE_FILES_PARAMS up -d
+./wait-up.sh "https://docker:8765/recebimento/info" 600
+
+gradle gulpTestE2E
+
+docker-compose $COMPOSE_FILES_PARAMS down
+docker stop grid

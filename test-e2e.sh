b3a3c373d906093ca0666f6f6893f79d2db8a820
Tomas.Godoi <tomas.godoi@stf.jus.br>
2016-07-01 00:48:48 -0300

    Tentando utilizar uma imagem menor do selenium.

--- test-e2e.sh
+++ test-e2e.sh
@@ -4,8 +4,7 @@ set -e
 MAIN_DOCKER_COMPOSE_FILE="-f docker-compose.yml"
 COMPOSE_FILES_PARAMS="$MAIN_DOCKER_COMPOSE_FILE -f docker-compose-e2e.yml"
 
-docker run -d --name=grid --net host -p 4444:24444 -p 5900:25900 -e VNC_PASSWORD=hola -v /dev/shm:/dev/shm elgalu/selenium:2.53.0s
-docker exec grid wait_all_done 30s
+docker run -d --name=selenium --net host -p 4444:4444 selenium/standalone-chrome:2.53.0
 
 docker-compose $COMPOSE_FILES_PARAMS up -d
 ./wait-up.sh "https://docker:8765/recebimento/info" 600
@@ -13,4 +12,5 @@ docker-compose $COMPOSE_FILES_PARAMS up -d
 gradle gulpTestE2E
 
 docker-compose $COMPOSE_FILES_PARAMS down
-docker stop grid
+docker stop selenium
+docker rm selenium

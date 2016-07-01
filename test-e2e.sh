0ef28ebe9b84d6b7d94dbf727e5b9149ae781c57
Tomas.Godoi <tomas.godoi@stf.jus.br>
2016-07-01 19:04:17 -0300

    Rodando todos os testes e2e.

--- test-e2e.sh
+++ test-e2e.sh
@@ -4,7 +4,7 @@ set -e
 MAIN_DOCKER_COMPOSE_FILE="-f docker-compose.yml"
 COMPOSE_FILES_PARAMS="$MAIN_DOCKER_COMPOSE_FILE -f docker-compose-e2e.yml"
 
-docker run -d --rm --name=selenium --net host -p 4444:4444 selenium/standalone-chrome:2.53.0
+docker run -d --name=selenium --net host -p 4444:4444 selenium/standalone-chrome:2.53.0
 
 docker-compose $COMPOSE_FILES_PARAMS up -d
 ./wait-up.sh "https://docker:8765/recebimento/info" 600
@@ -14,4 +14,4 @@ docker-compose $COMPOSE_FILES_PARAMS up -d
 gradle gulpTestE2E
 
 docker-compose $COMPOSE_FILES_PARAMS down
-docker stop selenium
+docker stop selenium && docker rm selenium

54661e9b5091599bf16679d45b57bfdfc85012ce
Tomás Godoi <tsmgodoi@gmail.com>
2016-07-02 12:06:28 -0300

    Mais um teste.

--- test-e2e.sh
+++ test-e2e.sh
@@ -6,9 +6,9 @@ COMPOSE_FILES_PARAMS="$MAIN_DOCKER_COMPOSE_FILE -f docker-compose-e2e.yml"
 
 docker run -d --name=selenium --net host -p 4444:4444 selenium/standalone-chrome:2.53.0
 
-docker-compose -f docker-compose-e2e.yml up -d
-#./wait-up.sh "https://docker:8765/recebimento/info" 600
-#./wait-up.sh "https://docker:8765/documents/info" 30
+docker-compose $COMPOSE_FILES_PARAMS up -d
+./wait-up.sh "https://docker:8765/recebimento/info" 600
+./wait-up.sh "https://docker:8765/documents/info" 30
 ./wait-up.sh "https://docker/OnlineEditorsExample/" 30
 
 free -m
@@ -17,5 +17,5 @@ docker stats --no-stream
 
 gradle gulpTestE2E
 
-docker-compose -f docker-compose-e2e.yml down
+docker-compose $COMPOSE_FILES_PARAMS down
 docker stop selenium && docker rm selenium

3440520e009fb9c52da0a357863270ac01d7917f
Tomás Godoi <tsmgodoi@gmail.com>
2016-07-04 11:42:44 -0300

    Movendo os scripts de diretório.

--- scripts/test-e2e.sh
+++ scripts/test-e2e.sh
@@ -5,12 +5,10 @@ COMPOSE_FILES_PARAMS="$MAIN_DOCKER_COMPOSE_FILE -f docker-compose-e2e.yml"
 
 docker-compose $COMPOSE_FILES_PARAMS up -d
 
-./wait-up.sh "https://docker:8765/recebimento/info" 600
-./wait-up.sh "https://docker:8765/documents/info" 30
-./wait-up.sh "https://docker/OnlineEditorsExample/" 30
-./wait-up.sh "http://docker:4444/wd/hub" 30
-
-docker stats --no-stream
+./scripts/wait-up.sh "https://docker:8765/recebimento/info" 600
+./scripts/wait-up.sh "https://docker:8765/documents/info" 30
+./scripts/wait-up.sh "https://docker/OnlineEditorsExample/" 30
+./scripts/wait-up.sh "http://docker:4444/wd/hub" 30
 
 gradle gulpTestE2E
 

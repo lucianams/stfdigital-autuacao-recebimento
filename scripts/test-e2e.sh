e85d2b7b4e4da2a5b4494f9bccc7ea4b05a6dfc6
Tomás Godoi <tsmgodoi@gmail.com>
2016-07-04 12:53:04 -0300

    Reestruturando os arquivos docker-compose.yml

--- scripts/test-e2e.sh
+++ scripts/test-e2e.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 MAIN_DOCKER_COMPOSE_FILE="-f docker-compose.yml"
-COMPOSE_FILES_PARAMS="$MAIN_DOCKER_COMPOSE_FILE -f docker-compose-e2e.yml"
+COMPOSE_FILES_PARAMS="$MAIN_DOCKER_COMPOSE_FILE -f docker-compose.e2e.yml"
 
 docker-compose $COMPOSE_FILES_PARAMS up -d
 

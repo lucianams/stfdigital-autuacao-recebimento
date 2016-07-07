2a080658f28ead53e0c3b5b13893158660d59d50
Rafael.Alencar <rafaelesdras@gmail.com>
2016-07-07 16:33:15 -0300

    Atualização para SpringBoot 1.4.0RC1.

--- src/test/java/br/jus/stf/autuacao/recebimento/RemessaOriginarioIntegrationTests.java
+++ src/test/java/br/jus/stf/autuacao/recebimento/RemessaOriginarioIntegrationTests.java
@@ -1,15 +1,33 @@
 package br.jus.stf.autuacao.recebimento;
 
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willDoNothing;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
-import br.jus.stf.core.framework.testing.IntegrationTestsSupport;
+import br.jus.stf.autuacao.recebimento.domain.model.Recebedor;
+import br.jus.stf.autuacao.recebimento.infra.DevolucaoRestAdapter;
+import br.jus.stf.autuacao.recebimento.infra.ProtocoloRestAdapter;
+import br.jus.stf.autuacao.recebimento.infra.RecebedorOauth2Adapter;
+import br.jus.stf.core.shared.documento.TextoId;
+import br.jus.stf.core.shared.identidade.PessoaId;
+import br.jus.stf.core.shared.protocolo.Numero;
+import br.jus.stf.core.shared.protocolo.Protocolo;
+import br.jus.stf.core.shared.protocolo.ProtocoloId;
 
 /**
  * Valida a API de registro de remessas.
@@ -21,8 +39,31 @@ import br.jus.stf.core.framework.testing.IntegrationTestsSupport;
  * @since 18.12.2015
  */
 @Ignore
-@SpringApplicationConfiguration(ApplicationContextInitializer.class)
-public class RemessaOriginarioIntegrationTests extends IntegrationTestsSupport {
+@RunWith(SpringRunner.class)
+@SpringBootTest({"server.port:0", "eureka.client.enabled:false"})
+@AutoConfigureMockMvc
+public class RemessaOriginarioIntegrationTests {
+
+	@MockBean
+	private DevolucaoRestAdapter devolucaoAdapter;
+	
+	@MockBean
+    private ProtocoloRestAdapter protocoloAdapter;
+	
+	@MockBean
+    private RecebedorOauth2Adapter recebedorAdapter;
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Before
+	public void configuracao() {
+		MockitoAnnotations.initMocks(this);
+		
+		given(protocoloAdapter.novoProtocolo()).willReturn(new Protocolo(new ProtocoloId(1L), new Numero(1L, 2016)));
+		given(recebedorAdapter.recebedor()).willReturn(new Recebedor("recebedor", new PessoaId(1L)));
+		willDoNothing().given(devolucaoAdapter).assinarTexto(new TextoId(9000L), "_DocTemp_12345");
+	}
 	
 	@Test
     public void registrarUmaRemessa() throws Exception {
@@ -31,10 +72,10 @@ public class RemessaOriginarioIntegrationTests extends IntegrationTestsSupport {
         
         result.andExpect(status().isOk());
     }
-	
+	@Ignore
 	@Test
     public void preautarUmaRemessa() throws Exception {
-        loadDataTests("preautarRemessaOriginario.sql");
+        //loadDataTests("preautarRemessaOriginario.sql");
         
         String remessaParaPreautuar = "{\"protocoloId\":@protocoloId, \"classeId\":\"ADI\", \"preferencias\":[3,8], \"sigilo\":\"PUBLICO\"}";
         String protocoloId = "9000";
@@ -42,10 +83,10 @@ public class RemessaOriginarioIntegrationTests extends IntegrationTestsSupport {
         
         result.andExpect(status().isOk());
     }
-	
+	@Ignore
 	@Test
     public void devolverUmaRemessa() throws Exception {
-		loadDataTests("devolverRemessaOriginario.sql");
+		//loadDataTests("devolverRemessaOriginario.sql");
 		
 		String remessaParaDevolver = "{\"protocoloId\":@protocoloId, \"motivo\":\"Remessa inválida.\"}";
         String protocoloId = "9001";
@@ -54,29 +95,29 @@ public class RemessaOriginarioIntegrationTests extends IntegrationTestsSupport {
         
         result.andExpect(status().isOk());
     }
-	
+	@Ignore
 	@Test
     public void prepararOficioDevolucaoDaRemessa() throws Exception {
-        loadDataTests("prepararOficioDevolucaoRemessaOriginario.sql");
+        //loadDataTests("prepararOficioDevolucaoRemessaOriginario.sql");
 		
-		String remessaParaPrepararOficio = "{\"protocoloId\":@protocoloId, \"motivo\":1, \"modeloId\":1, \"textoId\":1}";
+		String remessaParaPrepararOficio = "{\"protocoloId\":@protocoloId, \"motivo\":1, \"modeloId\":1, \"textoId\":9000}";
 		String protocoloId = "9002";
         ResultActions result = mockMvc.perform(post("/api/remessas/devolucao-oficio").contentType(APPLICATION_JSON).content(remessaParaPrepararOficio.replace("@protocoloId", protocoloId)));
         
         result.andExpect(status().isOk());
     }
-	
+	@Ignore
 	@Test
     public void assinarOficioDevolucaoDaRemessa() throws Exception {
-        loadDataTests("assinarOficioDevolucaoRemessaOriginario.sql");
+        //loadDataTests("assinarOficioDevolucaoRemessaOriginario.sql");
 		
-        String remessaParaAssinarOficio = "{\"protocoloId\":@protocoloId}";
+        String remessaParaAssinarOficio = "{\"protocoloId\":@protocoloId,\"documentoTemporarioId\":\"_DocTemp_12345\"}";
 		String protocoloId = "9003";
         ResultActions result = mockMvc.perform(post("/api/remessas/devolucao-assinatura").contentType(APPLICATION_JSON).content(remessaParaAssinarOficio.replace("@protocoloId", protocoloId)));
         
         result.andExpect(status().isOk());
     }
-	
+	@Ignore
 	@Test
     public void naoDeveRegistrarUmaRemessaInvalida() throws Exception {
         String remessaInvalida = "{\"formaRecebimento\":\"SEDEX\", \"apensos\":1, \"numeroSedex\":\"SR123456789BR\"}";
@@ -85,7 +126,7 @@ public class RemessaOriginarioIntegrationTests extends IntegrationTestsSupport {
         
         result.andExpect(status().isBadRequest());
     }
-
+	@Ignore
 	@Test
     public void naoDevePreautuarUmaRemessaInvalida() throws Exception {
         String remessaInvalida = "{\"protocoloId\":1, \"transicao\":\"devolver\", \"movito\":\"Remessa enviado ao STF antes de passar ao STJ\"}";
@@ -94,7 +135,7 @@ public class RemessaOriginarioIntegrationTests extends IntegrationTestsSupport {
         
         result.andExpect(status().isBadRequest());
     }
-
+	@Ignore
 	@Test
     public void naoDeveElaborarOficioParaDevolucaoDaRemessaInvalida() throws Exception {
         String remessaInvalida = "{}";
@@ -102,9 +143,8 @@ public class RemessaOriginarioIntegrationTests extends IntegrationTestsSupport {
         ResultActions result = mockMvc.perform(post("/api/remessas/devolucao-oficio").contentType(APPLICATION_JSON).content(remessaInvalida));
         
         result.andExpect(status().isBadRequest());
-        
     }
-
+	@Ignore
 	@Test
     public void naoDeveAssinarUmOficioDeDevolucao() throws Exception {
         String remessaInvalida = "{}";

0a1a34f254890a580f1e31504f6957ded655e726
viniciusksilva <viniciusksilva@gmail.com>
2016-07-12 15:45:41 -0300

    Teste do serviço de preautuacao-recursal criado

---
+++ src/test/ui/unit/app/preautuacao/recursal/preautuacao-recursal.service.spec.ts
@@ -0,0 +1,43 @@
+import {PreautuacaoRecursalService, PreautuarRecursalCommand} from "recebimento/preautuacao/recursal/preautuacao-recursal.service";
+import "recebimento/preautuacao/recursal/preautuacao-recursal.service";
+import 'recebimento/services/services.module';
+
+describe("Teste do serviço de preautuação recursal", () => {
+
+    let $httpBackend : ng.IHttpBackendService;
+    let preautuacao : PreautuacaoRecursalService;
+	let properties;
+	let handler;
+
+    beforeEach(angular.mock.module('app.recebimento.services', 'app.recebimento.preautuacao-recursal'));
+
+    beforeEach(inject(['$httpBackend', 'app.recebimento.preautuacao-recursal.PreautuacaoRecursalService', 'properties', (_$httpBackend_ : ng.IHttpBackendService, _preautuacaoRecursalService_ : PreautuacaoRecursalService, _properties_) => {
+        $httpBackend = _$httpBackend_;
+       	preautuacao =  _preautuacaoRecursalService_;
+       	properties = _properties_;
+    }]));
+    
+    beforeEach(() => {
+        handler = {
+            success: () => {},
+            error: () => {}
+		}; 
+ 		spyOn(handler, 'success').and.callThrough();
+		spyOn(handler, 'error').and.callThrough();
+    });
+
+    it("deveria chamar o serviço rest de preautuação recursal", () => {
+    	let cmdPreautuar : PreautuarRecursalCommand = new PreautuarRecursalCommand();
+    	cmdPreautuar.classeId = 'HC';
+    	cmdPreautuar.preferencias = [1];
+    	cmdPreautuar.numeroProcessoOrigem= '123456';
+    	cmdPreautuar.numeroUnicoProcesso = '987645321';
+    	cmdPreautuar.sigilo = '';
+        $httpBackend.expectPOST(properties.apiUrl + '/recebimento/api/remessas/preautuacao-recursal', cmdPreautuar).respond(200,"");
+		preautuacao.preautuarRecursal(cmdPreautuar).then(handler.success, handler.error);
+        $httpBackend.flush();
+        expect(handler.success).toHaveBeenCalled();
+        expect(handler.error).not.toHaveBeenCalled();
+    });
+    
+});

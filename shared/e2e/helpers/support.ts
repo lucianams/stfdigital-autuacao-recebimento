aee6a90f02674e8c17b7fe7957d9e9ccc416f9d7
Tomas.Godoi <tomas.godoi@stf.jus.br>
2016-07-30 00:48:58 -0300

    git subrepo pull shared
    subrepo:
      subdir:   "shared"
      merged:   "2362fe9"
    upstream:
      origin:   "https://github.com/supremotribunalfederal/stfdigital-shared"
      branch:   "master"
      commit:   "2362fe9"
    git-subrepo:
      version:  "0.3.0"
      origin:   "???"
      commit:   "???"

---
+++ shared/e2e/helpers/support.ts
@@ -0,0 +1,18 @@
+namespace support {
+    export function printBrowserLogs() {
+        browser.manage().logs().get('browser').then(function(browserLogs) {
+            browserLogs.forEach(function(log){
+                console.log(log.message);
+            });
+        });
+    }
+
+    export function scrollToElement(element: protractor.ElementFinder): webdriver.promise.Promise<{}> {
+        let scrollIntoView = function() {
+            arguments[0].scrollIntoView();
+        };
+        return browser.wait(browser.executeScript(scrollIntoView, element.getWebElement()));
+    }
+}
+
+export = support;

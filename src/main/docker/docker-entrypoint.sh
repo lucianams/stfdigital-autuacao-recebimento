c81613f26d28acbaf89a26d060e9a2ea19295029
Tomas.Godoi <tomas.godoi@stf.jus.br>
2016-08-03 21:06:10 -0300

    Configuração para usar o config-server.

--- src/main/docker/docker-entrypoint.sh
+++ src/main/docker/docker-entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 set -e
 
+# Aguarda o config-server subir para conseguir recuperar as configurações
+echo "Aguardando o discovery na porta 8761"
+while ! nc -z discovery 8761; do   
+	sleep 1 # Aguarda 1s
+done
+
 exec java -Djava.security.egd=file:/dev/./urandom $JAVA_OPTS -cp /app/ org.springframework.boot.loader.JarLauncher "$@"

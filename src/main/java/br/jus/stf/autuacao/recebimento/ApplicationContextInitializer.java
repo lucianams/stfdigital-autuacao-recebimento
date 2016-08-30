66bb5385246f66a037c64a33c708230c93504ed4
Tomas.Godoi <tomas.godoi@stf.jus.br>
2016-08-30 18:42:26 -0300

    Configurando o Hystrix.

--- src/main/java/br/jus/stf/autuacao/recebimento/ApplicationContextInitializer.java
+++ src/main/java/br/jus/stf/autuacao/recebimento/ApplicationContextInitializer.java
@@ -2,10 +2,13 @@ package br.jus.stf.autuacao.recebimento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.context.request.RequestContextListener;
 
 /**
  * @author Rodrigo Barreiros
@@ -18,7 +21,13 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @EnableResourceServer
 @EnableFeignClients
 @EnableEurekaClient
+@EnableCircuitBreaker
 public class ApplicationContextInitializer {
+	
+	@Bean
+	public RequestContextListener requestContextListener() {
+		return new RequestContextListener();
+	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ApplicationContextInitializer.class, args);

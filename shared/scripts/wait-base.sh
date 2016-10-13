ca3f087f45cc09c977d952a456e8555255e59638
Tomas.Godoi <tomas.godoi@stf.jus.br>
2016-10-13 18:32:10 -0300

    git subrepo clone https://github.com/supremotribunalfederal/stfdigital-shared shared
    subrepo:
      subdir:   "shared"
      merged:   "7b76b54"
    upstream:
      origin:   "https://github.com/supremotribunalfederal/stfdigital-shared"
      branch:   "master"
      commit:   "7b76b54"
    git-subrepo:
      version:  "0.3.0"
      origin:   "???"
      commit:   "???"

---
+++ shared/scripts/wait-base.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -e
+
+./shared/scripts/wait-up.sh "https://docker:8765/manage/info" 300
+./shared/scripts/wait-up.sh "https://docker:8765/discovery/manage/info" 300
+./shared/scripts/wait-up.sh "https://docker:8765/userauthentication/manage/info" 300

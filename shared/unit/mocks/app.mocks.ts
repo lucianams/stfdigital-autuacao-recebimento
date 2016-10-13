fc4f09b51d404c8d860c201d95841ffa452f04cd
Tomas.Godoi <tomas.godoi@stf.jus.br>
2016-10-13 18:40:42 -0300

    git subrepo clone --force https://github.com/supremotribunalfederal/stfdigital-shared shared
    subrepo:
      subdir:   "shared"
      merged:   "07a725f"
    upstream:
      origin:   "https://github.com/supremotribunalfederal/stfdigital-shared"
      branch:   "master"
      commit:   "07a725f"
    git-subrepo:
      version:  "0.3.0"
      origin:   "???"
      commit:   "???"

--- shared/unit/mocks/app.mocks.ts
+++ shared/unit/mocks/app.mocks.ts
@@ -1,31 +1,11 @@
 namespace app.core {
-	'use strict';
+    "use strict";
 
     angular
-        .module('app.core',
-            [
-//                'ngAnimate',
-//                'ngAria',
-                'ngCookies',
-//                'ngMessages',
-//                'ngResource',
-//                'ngSanitize',
-//                'ngMaterial',
-//                'angular-chartist',
-//                'chart.js',
-//                'datatables',
-//                'gridshore.c3js.chart',
-//                'nvd3',
-                'pascalprecht.translate',
-//                'timer',
-                'ui.router',
-//                'ct.ui.router.extras',
-//                'oc.lazyLoad',
-//                'ui.sortable',
-//                'ng-sortable',
-//                'xeditable',
-//                'classy',
-//                'ui.select'
+            .module("app.core", [
+                "ngCookies",
+                "pascalprecht.translate",
+                "ui.router"
             ]);
 }
 
@@ -34,84 +14,82 @@ namespace app.core {
  * 
  */
 namespace app.novoProcesso {
-	'use strict';
-	
+    "use strict";
+
     function config() {
 
-        
     }
 
     angular
-        .module('app.novo-processo', ['ui.router', 'pascalprecht.translate'])
-        .config(config);
+            .module("app.novo-processo", ["ui.router", "pascalprecht.translate"])
+            .config(config);
 }
 
 namespace app.documentos {
-	'use strict';
-	
+    "use strict";
+
     function config() {
 
-        
     }
 
     angular
-        .module('app.documentos', [])
-        .config(config);
+            .module("app.documentos", [])
+            .config(config);
 }
 
 namespace app.support {
-	'use strict';
-	
-    function config() { }
-    
-	let url = 'docker';
-	let port = "8765";
+    "use strict";
 
-	angular.module('app.support.constants', []).constant("properties", {
-	    "url": url,
-	    "port": port,
-	    "apiUrl": url + ":" + port,
-	    "development": true
-	});
-    
+    function config() { }
+
+    let url = "docker";
+    let port = "8765";
+
+    angular.module("app.support.constants", []).constant("properties", {
+        "url": url,
+        "port": port,
+        "apiUrl": url + ":" + port,
+        "development": true
+    });
+
     angular
-		.module('app.support.command', [])
-		.config(config);
+            .module("app.support.command", [])
+            .config(config);
 
     class CommandServiceMock {
-    	addValidator(){}
+        public addValidator() {}
     }
-    
+
     angular
-		.module('app.support.command')
-		.service('commandService', CommandServiceMock);
-    
+            .module("app.support.command")
+            .service("commandService", CommandServiceMock);
+
     class MessagesService {
-    	public error(message: string): void {
-    		
-    	}
-    	
-    	public success(message: string): void {
-    		
-    	}
+        public error(message: string): void {
+
+        }
+
+        public success(message: string): void {
+
+        }
     }
-    
-	angular
-	    .module('app.support.messaging', [])
-	    .service('messagesService', MessagesService)
-	    .config(config);
-
-	angular
-	    .module('app.certification', []);
-
-	angular
-	    .module('checklist-model', []);
 
     angular
-    	.module('app.support', ['app.support.command', 'app.support.messaging', 'app.support.constants'])
-    	.config(config);
+            .module("app.support.messaging", [])
+            .service("messagesService", MessagesService)
+            .config(config);
+
+    angular
+            .module("app.certification", []);
+
+    angular
+            .module("checklist-model", []);
+
+    angular
+            .module("app.support", ["app.support.command", "app.support.messaging", "app.support.constants"])
+            .config(config);
 }
 
-angular.module('angularFileUpload', []);
+angular.module("angularFileUpload", []);
 
-angular.module('ngCookies', []);
+angular.module("ngCookies", []);

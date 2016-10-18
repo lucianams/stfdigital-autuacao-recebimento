a96be43c321c3ac101f9d691549ba83d0fed39a4
Tomas.Godoi <tomas.godoi@stf.jus.br>
2016-10-18 16:16:57 -0200

    Setando o profile para o teste e2e.

--- shared/scripts/test-e2e.sh
+++ shared/scripts/test-e2e.sh
@@ -5,7 +5,7 @@ MAIN_DOCKER_COMPOSE_FILE="-f docker-compose${1:-}.yml"
 RUNNING_HOST="${2:-docker}"
 COMPOSE_FILES_PARAMS="$MAIN_DOCKER_COMPOSE_FILE -f shared/compose/docker-compose.e2e.base.yml -f compose/docker-compose.e2e.yml"
 
-EUREKA_PREFER_IP_ADDRESS=false docker-compose $COMPOSE_FILES_PARAMS up -d
+EUREKA_PREFER_IP_ADDRESS=false JAVA_SERVICE_SPRING_PROFILES=development,docker docker-compose $COMPOSE_FILES_PARAMS up -d
 
 ./shared/scripts/wait-base.sh $RUNNING_HOST
 ./scripts/wait-ready.sh $RUNNING_HOST

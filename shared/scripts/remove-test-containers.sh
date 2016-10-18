8c3fe58bee9b7c7d4f119511ae1c7f39526bd22d
Tomas.Godoi <tomas.godoi@stf.jus.br>
2016-10-18 16:36:15 -0200

    Removendo variável RUNNING_HOST no script remove.

--- shared/scripts/remove-test-containers.sh
+++ shared/scripts/remove-test-containers.sh
@@ -2,7 +2,6 @@
 set -e
 
 MAIN_DOCKER_COMPOSE_FILE="-f docker-compose${1:-}.yml"
-RUNNING_HOST="${2:-docker}"
 COMPOSE_FILES_PARAMS="$MAIN_DOCKER_COMPOSE_FILE -f shared/compose/docker-compose.e2e.base.yml -f compose/docker-compose.e2e.yml"
 
 docker-compose $COMPOSE_FILES_PARAMS down

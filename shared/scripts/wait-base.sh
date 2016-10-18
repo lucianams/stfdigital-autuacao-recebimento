6cd0b8418b7bb4c3f4876b15815b98d380d094a3
Rafael.Alencar <rafaelesdras@gmail.com>
2016-10-18 21:01:23 -0200

    git subrepo pull shared
    subrepo:
      subdir:   "shared"
      merged:   "d0c4f39"
    upstream:
      origin:   "https://github.com/supremotribunalfederal/stfdigital-shared"
      branch:   "master"
      commit:   "d0c4f39"
    git-subrepo:
      version:  "0.3.0"
      origin:   "???"
      commit:   "???"

--- shared/scripts/wait-base.sh
+++ shared/scripts/wait-base.sh
@@ -5,4 +5,4 @@ set -e
 
 ./shared/scripts/wait-up.sh "https://$1:8765/manage/info" 300
 ./shared/scripts/wait-up.sh "https://$1:8765/discovery/manage/info" 300
-./shared/scripts/wait-up.sh "https://$1:8765/userauthentication/manage/info" 300
+./shared/scripts/wait-up.sh "https://$1:8765/identidades/manage/info" 300

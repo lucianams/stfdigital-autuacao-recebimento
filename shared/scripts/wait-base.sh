bddff8bf2e6fabdfe680385bddedece5756031ae
Rafael.Alencar <rafaelesdras@gmail.com>
2016-10-19 21:09:49 -0200

    Correção do shared.

--- shared/scripts/wait-base.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-
-
-./shared/scripts/wait-up.sh "https://$1:8765/manage/info" 300
-./shared/scripts/wait-up.sh "https://$1:8765/discovery/manage/info" 300
-./shared/scripts/wait-up.sh "https://$1:8765/identidades/manage/info" 300
